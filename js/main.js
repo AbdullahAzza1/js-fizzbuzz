927e428da6ca2e1623634dca71a909180856de7d
AbdullahAzza1 <abdullahazza141@gmail.com>
2023-10-30 16:55:20 +0100

    step2

--- js/main.js
+++ js/main.js
@@ -1,15 +1,23 @@
 const contenuto = document.getElementById("container");
 
 for (let i = 1; i <= 100; i++) {
-    
+
     const contenuto = document.createElement("div");
     contenuto.innerHTML = `il valore e ${i}`;
 
-    if(i==3) { contenuto.classList.add ("Fizz")};
+    if (i == 3) {
+        contenuto.innerHTML = ("il valore e Fizz")
 
-    
+    } else if (i == 5) {
+        contenuto.innerHTML = ("il valore e Buzz")
+
+    }
+
+
+
+
     container.append(contenuto);
 
     console.log(i);
-    
+
 }
