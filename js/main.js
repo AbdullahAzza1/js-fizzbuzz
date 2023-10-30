1d07cb38a0aaa431a991b624407cfa2eed800b96
AbdullahAzza1 <abdullahazza141@gmail.com>
2023-10-30 15:57:46 +0100

    step1

--- js/main.js
+++ js/main.js
@@ -2,13 +2,13 @@ const contenuto = document.getElementById("container");
 
 for (let i = 1; i <= 100; i++) {
     
-    const nuovoParagraph = document.createElement("div");
-    nuovoParagraph.innerHTML = `il valore e ${i}`;
+    const contenuto = document.createElement("div");
+    contenuto.innerHTML = `il valore e ${i}`;
 
-    if(i==3) { nuovoParagraph.classList.add ("red")};
+    if(i==3) { contenuto.classList.add ("Fizz")};
 
     
-    container.append(nuovoParagraph);
+    container.append(contenuto);
 
     console.log(i);
     
